e96b12a7258c167adcc1777d8355efe5f23cb1d1
Ryan Chen <y.f.chenroderick@gmail.com>
2024-07-20 14:52:56 +0800

    init: project init

---
+++ server.js
@@ -0,0 +1,15 @@
+import { Server } from 'socket.io';
+
+const port = 3000;
+// io: Server Instance
+const io = new Server(port, {
+  // https://socket.io/docs/v4/handling-cors/
+  cors: {
+    origin: 'http://localhost:5173'
+  }
+});
+
+// io.of('/')
+io.on('connection', () => {
+  console.log('Connected to the socketIO');
+});
