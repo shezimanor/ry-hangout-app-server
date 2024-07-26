d0cc2e00172b715939f31c99fc400427ae7fbca1
Ryan Chen <y.f.chenroderick@gmail.com>
2024-07-26 23:50:59 +0800

    feat: add online user list

--- server.js
+++ server.js
@@ -9,16 +9,23 @@ const io = new Server(port, {
   }
 });
 
+// 訪客清單
+const connectedUsers = {};
+
 // io.of('/')
 io.on('connection', (socket) => {
-  console.log(`A user connected with id ${socket.id}`);
-
   // 註冊事件：訪客造訪
   socket.on('user-in', (userName) => {
-    // 發送事件（發給所有人除了觸發者）：訪客造訪
-    socket.broadcast.emit('user-in', userName);
+    console.log(`User: ${userName} has connected with id ${socket.id}`);
     // 保存 userName
     socket.userName = userName;
+    // 將訪客加入訪客清單
+    connectedUsers[socket.id] = userName;
+    // 發送事件（發給所有人除了觸發者）：訪客造訪
+    socket.broadcast.emit('user-in', userName);
+    // 發送事件：更新訪客清單
+    io.emit('user-list', connectedUsers);
+    console.log('user-list: ', connectedUsers);
   });
 
   // 註冊事件：用戶的群組訊息
@@ -28,13 +35,13 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('group-message', msg);
   });
 
-  // 註冊事件：用戶的群組訊息
+  // 註冊事件：使用者開始打字
   socket.on('user-typing', (user) => {
     // 發送事件（發給所有人除了觸發者）：顯示某個用戶正在打字的狀態
     socket.broadcast.emit('user-typing', user);
   });
 
-  // 註冊事件：用戶的群組訊息
+  // 註冊事件：使用者結束打字
   socket.on('user-not-typing', (user) => {
     // 發送事件（發給所有人除了觸發者）：取消某個用戶正在打字的狀態
     socket.broadcast.emit('user-not-typing', user);
@@ -42,8 +49,13 @@ io.on('connection', (socket) => {
 
   // 訪客離線
   socket.on('disconnect', () => {
-    console.log(`User with id ${socket.id} disconnected`);
+    console.log(`User: ${socket.name} with id ${socket.id} disconnected`);
+    // 將訪客移除訪客清單
+    delete connectedUsers[socket.id];
     // 發送事件（發給所有人除了觸發者）：訪客離開
     socket.broadcast.emit('user-out', socket.userName);
+    // 發送事件：更新訪客清單
+    io.emit('user-list', connectedUsers);
+    console.log('user-list: ', connectedUsers);
   });
 });
